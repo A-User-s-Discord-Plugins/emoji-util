e5eb7e4009637030bbcc409a85484819c7a2a5c8
abUwUser <pedro.santos.cartaxo@gmail.com>
2021-02-09 19:45:14 -0300

    why i let this here

--- index.jsx
+++ index.jsx
@@ -294,14 +294,3 @@ export default class EmojiUtil extends Plugin{
         </>
     }
 }
-
-Array.prototype.remove = function () { //why not?
-    var what, a = arguments, L = a.length, ax;
-    while (L && this.length) {
-        what = a[--L];
-        while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-        }
-    }
-    return this;
-};
